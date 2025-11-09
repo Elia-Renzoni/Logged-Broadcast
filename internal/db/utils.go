0b652944fcb87c68e8f896debd2e74d8884fff25
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-09 16:54:40 +0100

    refactor: update query syntax
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- internal/db/utils.go
+++ internal/db/utils.go
@@ -1,15 +1,54 @@
 package db
 
 const (
-	CREATE_DB string = "CREATE DB loggerb"
-	CREATE_TABLE_SENDER string = "CREATE TABLE IF NOT EXISTS Sender (senderID INTEGER,senderAddr TEXT NOT NULL,senderPort INTEGER NOT NULL,PRIMARY KEY(senderID));"
-	CREATE_TABLE_MESSAGE string = "CREATE TABLE IF NOT EXISTS Message (messageID INTEGER,messageEndpoint TEXT NOT NULL,messageKey TEXT NOT NULL,messageValue TEXT NOT NULL,PRIMARY KEY(messageID));"
-	CREATE_TABLE_BUFFER string =  "CREATE TABLE IF NOT EXISTS Buffer (logID INTEGER,logState INTEGER NOT NULL,senderID INTEGER NOT NULL,messageID INTEGER NOT NULL,PRIMARY KEY(logID),FOREIGN KEY (senderID) REFERENCES Sender(senderID),FOREIGN KEY (messageID) REFERENCES Message(messageID));"
-	INSERT_BUFFER string = "INSERT INTO Buffer (logState, senderID, messageID) VALUES (?, ?, ?);"
-	INSERT_SENDER string = "INSERT INTO Sender VALUES (senderAddr, senderPort) VALUES (?, ?);"
-	INSERT_MESSAGE string = "INSERT INTO Message VALUES (messageEndpoint, messageKey, messageValue) VALUES (?, ?);"
-	DELETE_MESSAGE string = "DELETE FROM Message WHERE messageKey = ?"
 	DELETE_SENDER string = "DELETE FROM Sender WHERE senderID = ?"
 	FETCH_MESSAGE_ID string = "SELECT Message.messageID FROM Message WHERE Message.messageKey = ?"
 	DELETE_ENTRIES_FROM_BUFFER string = "DELETE FROM Buffer WHERE messageID = ? AND senderID = ?"
 )
+
+var createDB string = `
+                        CREATE DB loggerb;
+		      `
+var createTableSender string = `
+                                 CREATE TABLE IF NOT EXISTS Sender (
+                                    senderID INTEGER,
+				    senderAddr TEXT NOT NULL,
+				    senderPort INTEGER NOT NULL,
+				    PRIMARY KEY(senderID)
+				 );
+                               `
+var createTableMessage string = `
+                                  CREATE TABLE IF NOT EXISTS Message (
+				     messageID INTEGER,
+				     messageEndpoint TEXT NOT NULL,
+				     messageKey TEXT NOT NULL,
+				     messageValue TEXT NOT NULL,
+				     PRIMARY KEY(messageID)
+				  );
+				`
+var createTableBuffer string = `
+				 CREATE TABLE IF NOT EXISTS Buffer (
+			             logID INTEGER,
+				     logState INTEGER NOT NULL,
+				     senderID INTEGER NOT NULL,
+				     messageID INTEGER NOT NULL,
+				     PRIMARY KEY(logID),
+				     FOREIGN KEY(senderID) REFERENCES Sender(senderID),
+				     FOREIGN KEY(messageID) REFERENCES Message(messageID)
+				 );
+                               `
+var insertBufferStmt string = `
+                                 INSERT INTO Buffer (logState, senderID, messageID) VALUES (?, ?, ?);
+			      `
+var insertSenderStmt string = `
+  				 INSERT INTO Sender (senderAddr, senderPort) VALUES (?, ?);
+                              `
+var insertMessageStmt string = `
+				 INSERT INTO Message (messageEndpoint, messageKey, messageValue)
+				 VALUES (?, ?, ?);
+			      `
+var deleteMessageStmt string = `
+ 				 DELETE FROM Message
+				 WHERE messageKey = ?;
+			       `
+
