e2eafd688de0bb50c8bbc47af59c7469391761ae
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-10 08:39:00 +0100

    fix(cmd): add init flags and basic aggregation before startup
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"log-b/internal/server"
+	"log-b/internal/cache"
+	"log-b/internal/db"
+	"flag"
 )
+
+type StartUp struct {
+	address string
+	listenPort string
+	seed bool 
+}
+
+var conf StartUp
+
+func init() {
+	var (
+		addr = flag.String("host", "127.0.0.1", "a string")
+		port = flag.String("port", "8080", "a string")
+		sFlag = flag.Bool("seed", false, "a bool")
+	)
+	flag.Parse()
+
+	conf = StartUp{
+		address:    *addr,
+		listenPort: *port,
+		seed:       *sFlag,
+	}
+}
 
 func startServer(node server.Replica) {
 	go node.BindTCP()
 	go node.ServeConns()
 }
 
-// TODO-> add command line flag parsing
 func main() {
-	node := server.NewLoggedServer()
+	inMemoryMap := cache.Bcache{}
+	diskStorage := db.NewDB()
+	node := server.NewLoggedServer(conf.address, conf.listenPort, inMemoryMap, diskStorage)
 	startServer(node)
 }
