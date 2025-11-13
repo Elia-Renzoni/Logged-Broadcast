b5c09c6b8826b6f933350029265738a8441c7783
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-13 13:17:08 +0100

    refactor(db): update indentation
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- internal/db/utils.go
+++ internal/db/utils.go
@@ -4,32 +4,32 @@ var createDB string = `
 CREATE DB logger
 `
 var createTableSender string = `
-                                CREATE TABLE IF NOT EXISTS Sender (
-                                    senderID INTEGER,
-				    senderAddr TEXT NOT NULL,
-				    senderPort INTEGER NOT NULL,
-				    PRIMARY KEY(senderID)
-				 );
+CREATE TABLE IF NOT EXISTS Sender (
+ senderID INTEGER,
+ senderAddr TEXT NOT NULL,
+ senderPort INTEGER NOT NULL,
+ PRIMARY KEY(senderID)
+);
                                `
 var createTableMessage string = `
-                                  CREATE TABLE IF NOT EXISTS Message (
-				     messageID INTEGER,
-				     messageEndpoint TEXT NOT NULL,
-				     messageKey TEXT NOT NULL,
-				     messageValue TEXT NOT NULL,
-				     PRIMARY KEY(messageID)
-				  );
+CREATE TABLE IF NOT EXISTS Message (
+ messageID INTEGER,
+ messageEndpoint TEXT NOT NULL,
+ messageKey TEXT NOT NULL,
+ messageValue TEXT NOT NULL,
+ PRIMARY KEY(messageID)
+);
 				`
 var createTableBuffer string = `
-				 CREATE TABLE IF NOT EXISTS Buffer (
-			             logID INTEGER,
-				     logState INTEGER NOT NULL,
-				     senderID INTEGER NOT NULL,
-				     messageID INTEGER NOT NULL,
-				     PRIMARY KEY(logID),
-				     FOREIGN KEY(senderID) REFERENCES Sender(senderID),
-				     FOREIGN KEY(messageID) REFERENCES Message(messageID)
-				 );
+CREATE TABLE IF NOT EXISTS Buffer (
+ logID INTEGER,
+ logState INTEGER NOT NULL,
+ senderID INTEGER NOT NULL,
+ messageID INTEGER NOT NULL,
+ PRIMARY KEY(logID),
+ FOREIGN KEY(senderID) REFERENCES Sender(senderID),
+ FOREIGN KEY(messageID) REFERENCES Message(messageID)
+);
                                `
 var insertBufferStmt string = `
                                  INSERT INTO Buffer (logState, senderID, messageID) VALUES (?, ?, ?);
