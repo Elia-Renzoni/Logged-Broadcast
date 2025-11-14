cc958b129457d236903aa5291244cdae0e8f36a2
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-14 08:51:15 +0100

    chore(broadcaster): add unit test for the broadcast primitive
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/broadcaster/spreader_test.go
@@ -0,0 +1,45 @@
+package broadcaster_test
+
+
+import (
+	"testing"
+	"log-b/internal/cluster"
+	"log-b/internal/broadcaster"
+	"net/http"
+)
+
+
+func formCluster() {
+	cluster.AddMember("127.0.0.1:8080")
+	cluster.AddMember("127.0.0.1:8081")
+	cluster.AddMember("127.0.0.1:8082")
+	cluster.AddMember("127.0.0.1:8083")
+}
+
+func TestDoBroadcast(t *testing.T) {
+	formCluster()
+	go startServers()
+	result := broadcaster.DoBroadcast([]byte("mockbytes"), broadcaster.SET_DATA, "/addbk")
+	
+	// fail the test if the majority quorum is 
+	// not reached
+	if !result {
+		t.Fail()
+	}
+}
+
+func startServers() {
+	group := cluster.GetFullMembershipList()
+	for _, node := range group {
+		go func(peer string) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/addbk", handleMessage)
+
+			http.ListenAndServe(peer, mux)
+		}(node)
+	}
+}
+
+func handleMessage(w http.ResponseWriter, r *http.Request) {
+	// TODO -> write header and basic text-encoded body message
+}
