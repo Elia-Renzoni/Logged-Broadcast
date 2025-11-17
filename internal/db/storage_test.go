d8510e26f473dd822b89aa7a9f2bed2ceb1c6837
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-17 08:38:36 +0100

    chore(db): add unit test
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/db/storage_test.go
@@ -0,0 +1,33 @@
+package db_test
+
+
+import (
+	"testing"
+	"log-b/internal/db"
+)
+
+
+// TODO
+func TestWriteMessage(t *testing.T) {
+	instance := db.NewDB()
+	instance.WriteMessage()
+}
+
+// TODO
+func TestDeleteMessage(t *testing.T) {
+	instance := db.NewDB()
+	instance.DeleteMessage()
+
+}
+
+// TODO
+func TestRetrieveMessage(t *testing.T) {
+	instance := db.NewDB()
+	instance.RetrieveMessage()
+}
+
+
+// TODO
+func TestDBFaultRecovery(t *testing.T) {
+
+}
