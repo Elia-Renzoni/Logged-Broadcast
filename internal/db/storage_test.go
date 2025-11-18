e56ddddaa01b53d1a7f22d83b5aca0ff8895deaf
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-18 08:51:29 +0100

    chore(db): complete test for write message func
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- internal/db/storage_test.go
+++ internal/db/storage_test.go
@@ -4,19 +4,34 @@ package db_test
 import (
 	"testing"
 	"log-b/internal/db"
+	"log-b/model"
 )
 
-// TODO
 func TestWriteMessage(t *testing.T) {
 	instance := db.NewDB()
-	instance.WriteMessage()
+	err := instance.StartDB()
+	defer instance.ShutdownDB()
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+	msg := model.PersistentMessage{
+		Sinfo: model.Sender{
+			Addr: "127.0.0.1",
+			Port: "8080",
+		},
+		Cinfo: model.MessageContent{
+			Endpoint: "/addbk",
+			Key:      "foo",
+			Value:    "bar",
+		},
+	}
+	instance.WriteMessage(msg, 0)
 }
 
 // TODO
 func TestDeleteMessage(t *testing.T) {
 	instance := db.NewDB()
 	instance.DeleteMessage()
-
 }
 
 // TODO
